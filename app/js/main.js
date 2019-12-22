c250477135e451f622caacd4b001de8d62d0f9a6
romvtk <romvtk@mail.ru>
2019-12-22 19:26:06 +0300

    Готовый макет с адаптацией до 320px

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
+$(function () {
+
 $('.ft-tab-icon').flexTabs({
-	// Параметры...
+	breakpoint: 500,
+	fade: 500
+	
+});
+
+$('.header__btn-menu').on('click', function () {
+	$('.menu ul').slideToggle();
+	})
 });
